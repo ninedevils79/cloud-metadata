4c18250fa25afe00df28e4b413d57e059c96fad5
Kumar Jadav <ninedevils79@users.noreply.github.com>
2016-08-31 11:17:06 +1000

    Added license

--- create_metadata.sh
+++ create_metadata.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-# Red Hat Consulting, Canberra
-# This will create the cloud-init metadata iso to be used for the KVM Images. 
-# Version 0.1 - 2016-07-27 - Initial release
-# Kumar Jadav - kjadav@redhat.com
-
+#Copyright 2016 Kumar Jadav
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 USER_DATA=user-data
 META_DATA=meta-data
